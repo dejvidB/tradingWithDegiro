da5ef873d635d566284fb39ceed4c149d8f68eb6
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-13 01:24:11 +0300

    Add pending order in transaction history

--- client/src/components/transactionHistory.js
+++ client/src/components/transactionHistory.js
@@ -2,22 +2,20 @@ import { Component } from 'react';
 import { List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 export default class TransactionHistory extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
     render(){
         return (
             <div style={{float: "right", minWidth: "80%"}}>
                 <div>
                     <span style={{float: "left"}}>Account total: 1000 EUR</span>
-                    <span style={{float: "right"}}>Today: </span>
+                    <span style={{float: "right"}}>Today: {this.props.orders.map(order => order.sell ? (order.sell - order.buy) * order.quantity : 0).reduce((a, b) => a + b, 0).toFixed(2)}</span>
                 </div>
                 <List>
                 {this.props.orders.map((order) => {
                     return(
+                    order.sell !== null ?
                     <ListItem key={order.id} style={{backgroundColor: (order.sell - order.buy) > 0 ? "#2e7d32" : "#d32f2f", color: "white"}}>
                         <ListItemIcon style={{color: "white"}}>
                             {(order.sell - order.buy) > 0 ? <AddCircleIcon /> : <RemoveCircleIcon />}
@@ -25,7 +23,17 @@ export default class TransactionHistory extends Component {
                         <ListItemText
                         primary={((order.sell - order.buy) * order.quantity).toFixed(2)}
                         secondary={order.symbol}/>
-                    </ListItem>);
+                    </ListItem>
+                    :
+                    <ListItem key={order.id} style={{backgroundColor: "lightgray", color: "white"}}>
+                        <ListItemIcon style={{color: "white"}}>
+                            <AccessTimeIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                        primary={order.symbol}
+                        secondary={order.quantity + " @ " + order.buy.toFixed(2)}/>
+                    </ListItem>
+                    );
                 })}
                 </List>
             </div>
