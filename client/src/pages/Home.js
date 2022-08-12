eb52006b97bb1169d636862a239f5a0bf76a5282
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-13 01:24:40 +0300

    Simulate buy and sell

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,9 +11,50 @@ export default class HomeComponent extends Component {
 
         this.state = {
             orders: [
-                {"id": 1, "symbol": "AAPL", "buy": 2.10, "sell": 2.09, quantity: 450},
-                {"id": 2, "symbol": "META", "buy": 2.10, "sell": 2.50, quantity: 450}
-            ]
+                // {"id": 1, "symbol": "AAPL", "buy": 2.10, "sell": 2.09, quantity: 450},
+                // {"id": 2, "symbol": "META", "buy": 2.10, "sell": 2.50, quantity: 450}
+            ],
+            orderExecution: false,
+            currentOrder: "sell",
+            productId: null
+        }
+
+        this.buy = (productId) => {
+            const newOrder = {"id": 3, "symbol": "NVDA", "buy": 180, "quantity": 10, "sell": null};
+            this.setState(state => ({...state, orderExecution: true}));
+            this.simulateOrder(() => this.setState(state => ({
+                ...state, 
+                orderExecution: false,
+                currentOrder: "buy",
+                productId,
+                orders: [newOrder, ...state.orders]})));
+        }
+
+        this.sell = (productId) => {
+            this.setState(state => ({...state, orderExecution: true}));
+            this.simulateOrder(() => this.setState(state => ({
+                ...state, 
+                orderExecution: false, 
+                currentOrder: "sell", 
+                productId,
+                orders: [
+                    {
+                        sell: 200,
+                        "id": state.orders[0].id, 
+                        "symbol": state.orders[0].symbol, 
+                        "buy": state.orders[0].buy, 
+                        "quantity": state.orders[0].quantity
+                    }
+                ].concat(state.orders.slice(1))
+            })));
+        }
+
+        this.simulateOrder = fallback => {
+            setTimeout(() => { fallback() }, 500);
+        }
+
+        this.setProductId = (productId) => {
+            this.setState(state => ({...state, productId}));
         }
     }
 
@@ -24,18 +65,22 @@ export default class HomeComponent extends Component {
                 <Container fixed style={{marginTop: "25px"}}>
                     <Grid container spacing={1}>
                         <Grid item xs={6} mt="1%">
-                            <SearchBar/>
+                            <SearchBar setProductId={this.setProductId} disabled={this.state.currentOrder === "buy"}/>
                             <Stack direction="row" spacing={2} mt="30%">
                                 <Button variant="contained"
                                     color="error" 
                                     size="large" 
-                                    startIcon={<PaidIcon />}>
+                                    startIcon={<PaidIcon />}
+                                    disabled={this.state.productId === null || this.state.orderExecution === true || this.state.currentOrder === "sell"}
+                                    onClick={() => this.sell(1)}>
                                     SELL
                                 </Button>
                                 <Button variant="contained"
                                     color="success" 
                                     size="large" 
-                                    startIcon={<ShoppingCartIcon/>}>
+                                    startIcon={<ShoppingCartIcon />}
+                                    disabled={this.state.productId === null || this.state.orderExecution === true || this.state.currentOrder === "buy"}
+                                    onClick={() => this.buy(1)}>
                                     BUY
                                 </Button>
                             </Stack>
