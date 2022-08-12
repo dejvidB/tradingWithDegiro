044c203cd0fe7d73162e78f4b600e0a5eec10d8c
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-12 23:54:53 +0300

    Create SearchBar component

---
+++ client/src/components/searchBar.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Box from '@mui/material/Box';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            stocks: [
+                {"id": "331868", "name": "Apple Inc",  "symbol": "AAPL"},
+                {"id": "331822", "name": "Meta Inc",  "symbol": "META"}
+            ]
+        }
+    }
+    
+    render(){
+        return (
+            <Autocomplete
+            options={this.state.stocks}
+            autoHighlight
+            getOptionLabel={(option) => option.symbol}
+            renderOption={(props, option) => (
+                <Box component="li" {...props}>
+                    {option.name} - {option.symbol}
+                </Box>
+            )}
+            renderInput={(params) => (
+                <TextField
+                {...params}
+                label="Find a stock"
+                inputProps={{
+                    ...params.inputProps
+                }}
+                />
+            )}
+            />
+        );
+    }
+}
