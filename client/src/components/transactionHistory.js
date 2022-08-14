a31a22f17ea575ea9471dbb122f5baab625a7f75
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-14 21:04:22 +0300

    Code style fixes

--- client/src/components/transactionHistory.js
+++ client/src/components/transactionHistory.js
@@ -9,32 +9,32 @@ export default class TransactionHistory extends Component {
         return (
             <div style={{float: "right", minWidth: "80%"}}>
                 <div>
-                    <span style={{float: "left"}}>Account total: 1000 EUR</span>
-                    <span style={{float: "right"}}>Today: {this.props.orders.map(order => order.sell ? (order.sell - order.buy) * order.quantity : 0).reduce((a, b) => a + b, 0).toFixed(2)}</span>
+                    <span style={{ float: "left" }}>Account total: {this.props.cash} {this.props.currency}</span>
+                    <span style={{ float: "right" }}>Today: {this.props.orders.map(order => order.sell ? (order.sell - order.buy) * order.quantity : 0).reduce((a, b) => a + b, 0).toFixed(2)}</span>
                 </div>
                 <List>
-                {this.props.orders.map((order) => {
-                    return(
-                    order.sell !== null ?
-                    <ListItem key={order.id} style={{backgroundColor: (order.sell - order.buy) > 0 ? "#2e7d32" : "#d32f2f", color: "white"}}>
-                        <ListItemIcon style={{color: "white"}}>
-                            {(order.sell - order.buy) > 0 ? <AddCircleIcon /> : <RemoveCircleIcon />}
-                        </ListItemIcon>
-                        <ListItemText
-                        primary={((order.sell - order.buy) * order.quantity).toFixed(2)}
-                        secondary={order.symbol}/>
-                    </ListItem>
-                    :
-                    <ListItem key={order.id} style={{backgroundColor: "lightgray", color: "white"}}>
-                        <ListItemIcon style={{color: "white"}}>
-                            <AccessTimeIcon />
-                        </ListItemIcon>
-                        <ListItemText
-                        primary={order.symbol}
-                        secondary={order.quantity + " @ " + order.buy.toFixed(2)}/>
-                    </ListItem>
-                    );
-                })}
+                    {this.props.orders.map((order) => {
+                        return (
+                            order.sell !== null ?
+                                <ListItem key={order.id} style={{ backgroundColor: (order.sell - order.buy) > 0 ? "#2e7d32" : "#d32f2f", color: "white" }}>
+                                    <ListItemIcon style={{ color: "white" }}>
+                                        {(order.sell - order.buy) > 0 ? <AddCircleIcon /> : <RemoveCircleIcon />}
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={((order.sell - order.buy) * order.quantity).toFixed(2)}
+                                        secondary={order.symbol} />
+                                </ListItem>
+                                :
+                                <ListItem key={order.id} style={{ backgroundColor: "lightgray", color: "white" }}>
+                                    <ListItemIcon style={{ color: "white" }}>
+                                        <AccessTimeIcon />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={order.symbol}
+                                        secondary={order.quantity + " @ " + order.buy.toFixed(2)} />
+                                </ListItem>
+                        );
+                    })}
                 </List>
             </div>
         );
