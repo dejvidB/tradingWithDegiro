ed5344a9ba9fbf48a633db70b44f6aa08f00a9bf
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-14 20:53:12 +0300

    Handle stock search, buy and sell

--- controllers/stock.js
+++ controllers/stock.js
@@ -1,48 +1,77 @@
 const degiroApi = require('degiro-api');
 const { DeGiroActions, DeGiroMarketOrderTypes, DeGiroTimeTypes, DeGiroProducTypes } = degiroApi.DeGiroEnums;
-const { OrderType } = degiroApi.DeGiroTypes;
 
-class Stock{
-    SEARCH_LIMIT = 10;
+const SEARCH_LIMIT = 10;
 
-    search = async (text) => {
-        const results = await degiro.searchProduct({
-            text,
-            type: DeGiroProducTypes.shares,
-            limit: this.SEARCH_LIMIT
+class Stock {
+    static search = (degiroInstance, text) => {
+        return new Promise((resolve, reject) => {
+            degiroInstance.searchProduct({
+                text,
+                type: DeGiroProducTypes.shares,
+                limit: SEARCH_LIMIT
+            }).then(results => {
+                resolve(results.filter(result => result.tradable)
+                    .map(result => ({
+                        id: result.id,
+                        name: result.name,
+                        symbol: result.symbol,
+                        currency: result.currency,
+                        closePrice: result.closePrice
+                    })));
+            }).catch(error => reject(error));
         });
-
-        return results.map(result => { result.id, result.name, result.symbol, result.currency });
     }
 
-    buy = async (productId, size, price) => {
-        const order: OrderType = {
-            buySell: DeGiroActions.BUY,
-            orderType: DeGiroMarketOrderTypes.LIMITED,
-            productId,
-            size,
-            timeType: DeGiroTimeTypes.DAY,
-            price
-        }
-
-        const { confirmationId, freeSpaceNew, transactionFees } = await degiro.createOrder(order);
-        const orderId = await degiro.executeOrder(order, confirmationId);
-        console.log(`Order executed with id: ${orderId}`)
+    static getPrice = symbol => {
+        return new Promise((resolve, reject) => {
+            fetch("https://finnhub.io/api/v1/quote?symbol=" + symbol + "&token=" + process.env.FINNHUB_API_KEY)
+                .then(response => response.json())
+                .then(response => resolve(response.c))
+                .catch(error => reject(error));
+        });
     }
 
-    sell = async (productId, size, price) => {
-        const order: OrderType = {
-            buySell: DeGiroActions.SELL,
-            orderType: DeGiroMarketOrderTypes.LIMITED,
-            productId,
-            size,
-            timeType: DeGiroTimeTypes.DAY,
-            price
-        }
+    static calculateTradeSize = (cash, stockPrice) => {
+        return Math.floor((cash - process.env.DEGIRO_TRANSACTION_FEES) / stockPrice);
+    }
 
-        const { confirmationId, freeSpaceNew, transactionFees } = await degiro.createOrder(order);
-        const orderId = await degiro.executeOrder(order, confirmationId);
-        console.log(`Order executed with id: ${orderId}`)
+    static buy = (degiroInstance, productId, price, size) => {
+        return new Promise((resolve, reject) => {
+            const order = {
+                buySell: DeGiroActions.BUY,
+                orderType: DeGiroMarketOrderTypes.LIMITED,
+                productId,
+                size,
+                timeType: DeGiroTimeTypes.DAY,
+                price
+            }
+
+            degiroInstance.createOrder(order).then(({ confirmationId, freeSpaceNew, transactionFees }) => {
+                degiroInstance.executeOrder(order, confirmationId).then(orderId => {
+                    resolve(orderId);
+                })
+            }).catch(error => reject(error));
+        });
+    }
+
+    static sell = async (degiroInstance, productId, price, size) => {
+        return new Promise((resolve, reject) => {
+            const order = {
+                buySell: DeGiroActions.SELL,
+                orderType: DeGiroMarketOrderTypes.STOP_LOSS,
+                productId,
+                size,
+                timeType: DeGiroTimeTypes.DAY,
+                price
+            }
+
+            degiroInstance.createOrder(order).then(({ confirmationId, freeSpaceNew, transactionFees }) => {
+                degiroInstance.executeOrder(order, confirmationId).then(orderId => {
+                    resolve(orderId);
+                })
+            }).catch(error => reject(error));
+        });
     }
 }
 
