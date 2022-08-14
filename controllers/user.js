6f4e307c7d0c5ddc869d67870e95095821769f0a
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-14 20:58:17 +0300

    Retrieve cash funds

--- controllers/user.js
+++ controllers/user.js
@@ -1,34 +1,12 @@
-const DeGiro = require('degiro-api').default;
-
-class User{
-    constructor(){
-        this.username = '';
-        this.password = '';
-    }
-
-    login = async () => {
-        const degiro = DeGiro.create({ 
-            username: this.username,
-            pwd: this.password,
-            jsessionId: this.jsessionId 
+class User {
+    static getCashFunds = degiroInstance => {
+        return new Promise((resolve, reject) => {
+            degiroInstance.getCashFunds().then(cashFunds => {
+                resolve(cashFunds.filter(cash => cash.value > 0));
+            }, error => {
+                reject(error);
+            });
         });
-
-        if(!await degiro.isLogin({ secure: true })){
-            await degiro.login();
-            this.jsessionId = degiro.getJSESSIONID();
-            this.degiroInstance = degiro;
-
-            console.log("Logged in with session id: " + this.jsessionId);
-        }
-
-        return this.jsessionId;
-    }
-
-    loadData = async () => {
-        const cashFunds = await this.degiroInstance.getCashFunds();
-        console.log(JSON.stringify(cashFunds, null, 2));
-
-        
     }
 }
 
