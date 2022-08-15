a09b0e59197c0291e62b285d43cc0214ce8e5f7b
Dejvid Bushi <dejvid.bushi@gmail.com>
2022-08-15 14:29:00 +0300

    Execute buy/sell orders

--- routes/stocks.js
+++ routes/stocks.js
@@ -4,27 +4,33 @@ const Stock = require('../controllers/stock');
 const Util = require('../controllers/util');
 
 router.post('/buy', function (req, res, next) {
-    Util.convertCurrency(req.body.cashCurrency, req.body.stockCurrency, req.body.cash)
+    Util.convertCurrency(req.body.cashCurrency - process.env.DEGIRO_TRANSACTION_FEES, req.body.stockCurrency, req.body.cash)
         .then(convertedCash => {
-            Stock.getPrice(req.body.symbol).then(stockPrice => {
-                // Stock.buy(req.degiroInstance, req.body.stockId, stockPrice, Stock.calculateTradeSize(convertedCash, stockPrice));
-                res.json({
-                    orderId: 1,
-                    price: stockPrice,
-                    quantity: Stock.calculateTradeSize(convertedCash, stockPrice)
-                });
-            })
-        });
+            Stock.getPrice(req.body.symbol)
+                .then(price => {
+                    const quantity = Stock.calculateTradeSize(convertedCash, price);
+                    Stock.buy(req.degiroInstance, req.body.stockId, price, quantity)
+                        .then(orderId => {
+                            return res.json({
+                                orderId,
+                                price,
+                                quantity
+                            });
+                        })
+                })
+        })
 });
 
 router.post('/sell', function (req, res, next) {
     Stock.getPrice(req.body.symbol)
         .then(stockPrice => {
-            // Stock.sell(req.degiroInstance, req.body.stockId, stockPrice, req.body.quantity);
-            res.json({
-                orderId: 2,
-                price: stockPrice
-            });
+            Stock.sell(req.degiroInstance, req.body.stockId, stockPrice, req.body.quantity)
+                .then(orderId => {
+                    return res.json({
+                        orderId: orderId,
+                        price: stockPrice
+                    });
+                })
         })
 });
 
